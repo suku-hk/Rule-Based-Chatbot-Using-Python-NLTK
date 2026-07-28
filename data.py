f8f3c5afd53bd93117aa6d49c7939869c09388e0
suku-hk <sushmasushma2006@gmail.com>
2026-07-28 17:06:03 +0530

    Create data.py

---
+++ data.py
@@ -0,0 +1,10 @@
+data = {
+    "greetings" : ["hello","hi","hey","howdy","hola","good morning","good afternoon","good evening","what's up","yo","good night"],
+    "responses" : ["Hello!","Hi there!","Hey!","Greetings!","How can i help you?","Nice to see you!","HI ,how are you?","Hey ,what's going on?"],
+    "farewells" : ["bye","goodbye","see you later","take care","farewell"," have a good day","catch you later"],
+    "farewell_responses" : ["Goodbye!","See you later!","Take care!","Have a great day!","Bye ,Come back soon!","Farewell!"],
+    "questions" :["how are you","what's your name","what can you do","who created you","what's the weather today"],
+    "question_responses" :["I'm just a bot , but i'm doing great!","I'm your friendly chatbot!","I can answer questions and chat with you.","I was created by S.KALYAN"],
+    "small_talk" :["tell me a joke","what's new","how's it going","what are you up to"],
+    "small_talk_responses" :["Why don't scientist trust atoms? Because they make up everything!","Not much,just here to chat with you!","It's going well,Thanks!"]
+ }
